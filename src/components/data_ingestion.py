053cdbda749291b49eb32b7c25b6acdfd25feb43
Mudassir4545 <muda1997ssir@gmail.com>
2023-06-01 16:55:29 +0500

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,10 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTranformation
 from src.components.data_transformation import DataTranformationConfig
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -49,6 +53,9 @@ if __name__ == "__main__":
     train_data,test_data=obj.initiate_data_injestion()
 
     data_transformation=DataTranformation()
-    data_transformation.initiate_data_tranformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_tranformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
 
